e9e4d1f25ab5743b1b24471af01ac5f3010c48f3
Arzu <arzu.gojayeva03@gmail.com>
2023-02-23 12:45:15 +0400

    initial commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,63 @@
+﻿// See https://aka.ms/new-console-template for more information
+#region Task 1
+
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("Zehmet olmasa ayi daxil edin:");
+//string month = Console.ReadLine();
+//int n = month switch
+//{
+//    "January" or "April" or "May" or"June"or "September" => 30,
+//    "February" or "July" or "August"or "November" => 31,
+//_=> 28,//march 
+//"april"=>30,
+//"May"=>30,
+//"June" => 30,
+//"July" => 31,
+//"August" => 31,
+//"September" => 30,
+//"November" => 31,
+//_ => 31
+
+//};
+//Console.WriteLine(n);
+#endregion
+
+#region Task2
+//Console.WriteLine("Zehmet olmasa texti daxil edin:");
+//string word=Console.ReadLine();
+//Console.WriteLine("Zehmet olmasa herfi daxil edin:");
+//string firstLetter  = Console.ReadLine();
+
+//if (firstLetter.Length != 1)
+//{
+//    Console.WriteLine("Bir herif daxil etmelisiniz ");
+//}
+//else
+//{
+//    if (firstLetter == "")
+//    {
+//        Console.WriteLine("Bos kecile bilmez");
+//    }
+//    else
+//    {
+//        char newLetter = char.Parse(firstLetter);
+//        int count = 0;
+
+//        for (int i = 0; i < word.Length; i++)
+//        {
+//            if (word[i] == newLetter)
+//            {
+//                count++;
+//            }
+//        }
+//        Console.WriteLine(count);
+
+//    }
+
+//}
+#endregion
+
+#region Task3
+
+
+#endregion
---
+++ first/main.js
@@ -0,0 +1,39 @@
+let a=10;
+let b=20;
+let z;
+//Arithmetic Operators
+console.log(a+b);
+console.log(a-b)
+console.log(a*b)
+console.log(a**b)
+console.log(a/b)
+console.log(a%b)
+console.log(a++)
+console.log(a--)
+console.log(--b)
+console.log(++b)
+//Assignment Operators
+console.log(a+=b)
+console.log(a-=b)
+console.log(a/=b)
+console.log(a*=b)
+console.log(a%b)
+console.log(a**=b)
+// Comparison Operators
+console.log(a==b)
+console.log(a===b)
+console.log(a!==b)
+console.log(a!=b)
+console.log(a>=b)
+console.log(a<=b)
+console.log(a>b)
+console.log(a<b)
+console.log(z=(a==b)?10:20)
+// Logical Operators
+console.log(a==b&&a==z);
+console.log(a==b||a==z);
+//adding operators
+let text1 = "Yusif";
+let text2 = "Eliyev";
+let text3 = text1 + " " + text2;
+console.log(text3)
